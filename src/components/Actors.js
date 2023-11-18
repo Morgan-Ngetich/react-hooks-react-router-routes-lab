294cd832278bb05b0cc3e9c9dda0e81599dc4f0e
Morgan-Ngetich <ngetichmorgan6@gmail.com>
2023-11-18 17:09:33 +0300

    Completed assignment

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -2,7 +2,26 @@ import React from "react";
 import { actors } from "../data";
 
 function Actors() {
-  return <div>{/*{code here}*/}</div>;
+  console.log(actors)
+  return (
+    <div>
+    <h1>Actors Page</h1>
+    {actors.map((actor, actorIndex) => (
+      <div key={actorIndex}>
+        <p> {actor.name}</p>
+        <ul>
+          {actor.movies.map((movie, movieIndex) => (
+            
+              <li key={movieIndex}>
+                {movie}
+              </li>
+           
+          ))}
+          </ul>
+      </div>
+    ))}
+  </div>
+  )
 }
 
 export default Actors;
